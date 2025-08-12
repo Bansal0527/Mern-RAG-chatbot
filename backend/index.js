a1785379099ff402eecf89288f0770827e8285e1
Bansal0527 <mayank.bansal.0527@gmail.com>
2025-08-12 15:07:57 +0530

    routes for documents (upload, get , delete)

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors'); // Import cors
 const authRouter = require('./routes/Auth'); // Import auth routes
 require('dotenv').config();
-
+const documentRouter = require('./routes/Document');
 // Import database connection
 const connectDB = require('./config/database');
 
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/document', documentRouter); 
 
 
 
