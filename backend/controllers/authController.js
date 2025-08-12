b6a86d37803121d8a7fbd86e96d6d27110dbdb54
Bansal0527 <mayank.bansal.0527@gmail.com>
2025-08-12 12:38:44 +0530

    authentication routes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,88 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email)
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const user = new User({
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+
+    return res.status(201).json({
+      user: {
+        id: user._id,
+        email: user.email,
+        role: user.role
+      },
+      token
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating user' });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Verify password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+
+    res.json({
+      user: {
+        id: user._id,
+        email: user.email,
+        role: user.role
+      },
+      token
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error logging in' });
+  }
+};
+
+
+
+module.exports = {
+  register,
+  login,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); // Import cors
-
+const authRouter = require('./routes/Auth'); // Import auth routes
 require('dotenv').config();
 
 // Import database connection
@@ -17,6 +17,7 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use('/api/v1/auth', authRouter);
 
 
 
